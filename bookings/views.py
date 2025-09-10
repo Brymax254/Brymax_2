1c617d5634c244a22752aa2f64d41ce0dda9b9d8
Francis Ndegwa Muchemi <francisbrymax@gmail.com>
2025-09-10 10:11:55 +0300

    Fix: add HttpResponse import for pesapal_callback

--- bookings/views.py
+++ bookings/views.py
@@ -11,7 +11,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
