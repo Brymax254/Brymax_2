ca85859f81d72acbd210b5305aeb5ae22dc9d2fa
Francis Ndegwa Muchemi <francisbrymax@gmail.com>
2025-09-17 12:25:33 +0300

    Add div filter for receipt calculations

--- bookings/templatetags/math_filters.py
+++ bookings/templatetags/math_filters.py
@@ -4,7 +4,16 @@ register = template.Library()
 
 @register.filter
 def mul(value, arg):
+    """Multiply the value by the arg."""
     try:
         return float(value) * float(arg)
     except (ValueError, TypeError):
-        return ''
+        return ""
+
+@register.filter
+def div(value, arg):
+    """Divide the value by the arg."""
+    try:
+        return float(value) / float(arg)
+    except (ValueError, TypeError, ZeroDivisionError):
+        return ""
